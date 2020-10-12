18a5949f1d017c441cce9fca0d9db12a949f66f8
Victor de Carvalho Silva <vtrcarvalho.13@gmail.com>
2020-10-12 16:46:48 -0300

    CSS Custom

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -25,10 +25,10 @@ function OrphanagesMap(){
                 </footer>
             </aside>
 
-            <Map center={[-18.414425,-18.414425,]} zoom={15} style={{width: '100%', height: '100%'}}>
+            <Map center={[-18.4144052,-40.2247982,]} zoom={15} style={{width: '100%', height: '100%'}}>
                 {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/> */}
 
-                <TileLayer url=""/>
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
             </Map>
 
 
